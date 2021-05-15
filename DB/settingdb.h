782670161b7efaabcf0ee56a9bcccbaac29f6de8
Saiu <34592712+Snsaiu@users.noreply.github.com>
2021-05-15 21:36:17 +0800

    add setting

--- DB/settingdb.h
+++ DB/settingdb.h
@@ -1,11 +1,41 @@
 #ifndef SETTINGDB_H
 #define SETTINGDB_H
 
+#include <Models/settingmodel.h>
+
+
 
 class SettingDb
 {
 public:
     SettingDb();
+
+    ///
+    /// \brief GetSetting 获得设置内容
+    /// \return
+    ///
+    SettingModel GetSetting();
+
+    ///
+    /// \brief SetIdentityCount 设置身份鉴别次数
+    /// \return
+    ///
+    bool SetIdentityCount(int count);
+
+    ///
+    /// \brief SetPwdChangeCycle 设置口令修改周期
+    /// \param count
+    /// \return
+    ///
+    bool SetPwdChangeCycle(int count);
+
+    ///
+    /// \brief SetLogSaveCycle 设置日志保存最短周期
+    /// \param count
+    /// \return
+    ///
+    bool SetLogSaveCycle(int count);
+
 };
 
 #endif // SETTINGDB_H
